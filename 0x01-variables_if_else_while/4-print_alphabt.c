ff294a2b96a89ebefe00d7f6c6cbe7c285a81626
hullaah <113603028+Hullaah@users.noreply.github.com>
2023-03-17 18:43:37 +0100

    solved all the mandatory tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int character = 97;
+while (character < 123)
+{
+
+if (character == 'q' || character == 'e');
+else
+{
+putchar(character);
+}
+
+character++;
+}
+putchar(10);
+    
+return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int number = 48;
+
+while (number < 58)
+{
+putchar(number);
+if (number == 57);
+else
+{
+putchar(44);
+}
+putchar(32);
+number++;
+}
+
+putchar(10);
+    
+return (0);
+}
