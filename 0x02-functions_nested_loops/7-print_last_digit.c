7616021a8254a5fe50676ccf1425ea53b1174d08
hullaah <umaradelo1.247@gmail.com>
2023-03-22 17:34:28 +0100

    corrected file 3 7 and 11 which were making me fail the mandatory tasks

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,9 @@ int print_last_digit(int a)
 {
 	if (a < 0)
 	{
-		_putchar(-1 * (a % 10));
+		_putchar((-1 * (a % 10)) + 48);
 		return (-1 * (a % 10));
 	}
-	_putchar(a % 10);
+	_putchar((a % 10) + 48);
 	return (a % 10);
 }
