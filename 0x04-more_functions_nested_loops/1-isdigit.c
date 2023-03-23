c34ed407c4146519f429debd5783f78e3e527a82
hullaah <umaradelo1.247@gmail.com>
2023-03-23 09:57:07 +0100

    added file 1 and 0 to the directory

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isdigit - checkes if a character is a digit.
+*@n: character
+*Return: 0 if character is a digit otherwise 1
+*/
+int _isdigit(int c)
+{
+	if (i > 47 && i < 59)
+		return (0);
+	else
+		return (1);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# 0x04-more_functions_nested_loops
+
+This is the directory for more functions and nested lopps project done during the alx software engineering course.
