d18355b0186ccaa5c7af61e89c2bb33eebe6cb1d
hullaah <umaradelo1.247@gmail.com>
2023-03-23 21:56:19 +0100

    made some quick fixes to file 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,6 +14,10 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -22,4 +26,8 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
 	}
+}
+int main(void)
+{
+	return 0;
 }
