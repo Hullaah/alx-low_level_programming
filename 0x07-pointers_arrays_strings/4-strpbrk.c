288b05913e4ea03f5492cc292c876d6d6f9618b0
hullaah <umaradelo1.247@gmail.com>
2023-04-03 22:26:04 +0100

    corrected the name of the directory

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strpbrk - 
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i,j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			
+		}
+		
+	}
+	
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strstr - 
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
