7d76399af390ff14cfce7268df0072b745ab995d
hullaah <umaradelo1.247@gmail.com>
2023-04-05 22:28:02 +0100

    solved all the mandatory tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,34 @@
 #include "main.h"
 /**
+ * _sqrt_recursion_base - base for square root recursion
+ * @n: number
+ * @i: half of n
+ * Return: square root of n
+*/
+int _sqrt_recursion_base(int n, int i)
+{
+	float ans = (float) n / i;
+
+	if (ans == i)
+	{
+		return (i);
+	}
+	if (ans > i)
+	{
+		return (-1);
+	}
+	else
+	{
+		i--;
+		return (_sqrt_recursion_base(n, i));
+	}
+}
+/**
  * _sqrt_recursion - determines the square root of a number
  * @n: number
  * Return: square root of n
 */
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	return (0);
+	return (_sqrt_recursion_base(n, n / 2));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,14 +1,41 @@
 #include "main.h"
 /**
+ * _is_base_prime - base for the function is_prime_number
+ * @n: number
+ * @i: base number
+ * Return: 1 if number is a prime number 0 otherwise
+*/
+int _is_base_prime(int n, int i)
+{
+	if (n / i == 1)
+	{
+		return (1);
+	}
+	if (n % i || (n / i) == n)
+	{
+		i++;
+		return (_is_base_prime(n, i));
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
  * is_prime_number - determines if a number is a prime number
  * @n: number
  * Return: 1 if number is a prime number 0 otherwise
 */
 int is_prime_number(int n)
 {
-	if (n % n)
+	if (n == 1 || n == -1)
 	{
-		/* code */
+		return (0);
 	}
-	return (1);
+	if (n < 0)
+	{
+		return (_is_base_prime(n * -1, 1));
+	}
+	return (_is_base_prime(n, 1));
 }
+
