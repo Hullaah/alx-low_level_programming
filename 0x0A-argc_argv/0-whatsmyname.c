1e21f59221aeda76d40ab07c50fca50d72133978
hullaah <umaradelo1.247@gmail.com>
2023-04-07 13:37:50 +0100

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of a program
+ * @rgc: argument count
+ * @argv: argument array
+ * Return: Always 0 (success)
+*/
+int main(char argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
