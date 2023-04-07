42cb7ac5df58f3eccfb94404feb025df62beb781
hullaah <umaradelo1.247@gmail.com>
2023-04-07 20:33:04 +0100

    removed compiler warnings for unused variables

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 /**
  * main - prints the name of a program
- * @rgc: argument count
+ * @argc: argument count
  * @argv: argument array
  * Return: Always 0 (success)
 */
-int main(char argc, char *argv[])
+int main(int __attribute__((unused))argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
