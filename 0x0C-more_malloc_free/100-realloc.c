e166492b6872cfaa9f5c02f2bbfc47b97424c357
hullaah <umaradelo1.247@gmail.com>
2023-04-14 12:17:58 +0100

    added the file 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -44,7 +44,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-        tmp = malloc(new_size);
+	tmp = malloc(new_size);
 	_memcpy(tmp, ptr, old_size < new_size ? old_size : new_size);
 	free(ptr);
 	return (tmp);
