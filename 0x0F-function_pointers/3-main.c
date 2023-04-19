ebeab89ae3bf4a767d4e68e1b32ecefb654bdc41
hullaah <umaradelo1.247@gmail.com>
2023-04-19 18:06:24 +0100

    forcibly added the 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * main - calculates the operation on a given number
+ * supplied via the command-line
+ * @argc: command-line argument count
+ * @argv: array of command-line argument
+ * Return: check the code
+*/
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int ans;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		printf("%d\n", argc);
+		exit(98);
+	}
+	if (argv[2][0] != '+' && argv[2][0] != '-' &&
+	argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' && argv[3][0] == '0') ||
+	(argv[2][0] == '%' && argv[3][0] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	ans = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", ans);
+	return (0);
+}
