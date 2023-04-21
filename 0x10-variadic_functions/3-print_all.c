4b7f0efe94fae334a12ca4323af21a95efb8e988
hullaah <umaradelo1.247@gmail.com>
2023-04-21 23:06:52 +0100

    made some changes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,14 +1,17 @@
 #include "variadic_functions.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * print_all - prints all the arguments given to it separating them with comma
+ * @format: format that specifies how to print the string
+ * Return: void (does not have arteturn value)
+*/
 void print_all(const char * const format, ...)
 {
 	char c;
 	char *s;
 	int i, j = 0;
 	float f;
-	/*int status = 0;*/
 	va_list args;
 
 	va_start(args, format);
@@ -19,18 +22,26 @@ void print_all(const char * const format, ...)
 		case 'c':
 			c = (char) va_arg(args, int);
 			printf("%c", c);
+			if (format[j + 1] != '\0')
+				printf(", ");
 			break;
 		case 'f':
 			f = (float) va_arg(args, double);
 			printf("%f", f);
+			if (format[j + 1] != '\0')
+				printf(", ");
 			break;
 		case 's':
 			s = va_arg(args, char *);
 			printf("%s", s);
+			if (format[j + 1] != '\0')
+				printf(", ");
 			break;
 		case 'i':
 			i = va_arg(args, int);
 			printf("%d", i);
+			if (format[j + 1] != '\0')
+				printf(", ");
 			break;
 		default:
 			break;
