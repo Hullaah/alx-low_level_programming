0690df92ff625d5bf3da827f08af8c1bac10e24f
hullaah <umaradelo1.247@gmail.com>
2023-04-21 06:17:00 +0100

    added file 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,52 +2,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct
-{
-	char type;
-	void (*f)(void *type);
-} print;
-
 void print_all(const char * const format, ...)
 {
-	char *traverse = format;
 	char c;
 	char *s;
-	int i;
+	int i, j = 0;
 	float f;
-	int status;
-	print elements[] = {
-			{ 'c', print_char },
-			{ 's', print_string },
-			{ 'f', print_float },
-			{ 'i', print_int}
-		}
+	/*int status = 0;*/
 	va_list args;
 
 	va_start(args, format);
-	while (*traverse != '\0')
+	while (format[j] != '\0')
 	{
-		
+		switch (format[j])
+		{
+		case 'c':
+			c = (char) va_arg(args, int);
+			printf("%c", c);
+			break;
+		case 'f':
+			f = (float) va_arg(args, double);
+			printf("%f", f);
+			break;
+		case 's':
+			s = va_arg(args, char *);
+			printf("%s", s);
+			break;
+		case 'i':
+			i = va_arg(args, int);
+			printf("%d", i);
+			break;
+		default:
+			break;
+		}
+		j++;
 	}
-}
-void print_char(void *c)
-{
-	char d = *(char *) c;
-	printf("%c", d);
-}
-
-void print_string(void *s)
-{
-	char *t = (char *) s;
-	printf("%s", t);
-}
-void print_float(void *f)
-{
-	float g = *(float *) f;
-	printf("%f", g);
-}
-void print_int(void *i)
-{
-	int f = *(int *) i;
-	printf("%d", f);
 }
