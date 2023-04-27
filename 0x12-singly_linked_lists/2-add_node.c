972867ca55681a5195c005f95723e575c7ca354a
hullaah <umaradelo1.247@gmail.com>
2023-04-27 20:11:20 +0100

    added the file 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,46 @@
 #include "lists.h"
+#include <string.h>
 
+/**
+ * _strlen - calculates the length of a string
+ * @s: s
+ * Return: the length of a string
+*/
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - adds a node to a singly linked list
+ * @head: pointer to node start
+ * @str: node string
+ * Return: pointer to new node
+*/
 list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *node;
+	char *string;
+	int string_length;
+
+	string_length = _strlen(str);
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+	string = strdup(str);
+	if (string == NULL)
+		return (NULL);
+	node->str = string;
+	node->len = string_length;
+	node->next = *head;
+
+	*head = node;
+	return (*head);
 }
