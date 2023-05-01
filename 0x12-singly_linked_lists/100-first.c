f7b678e0937fb487be5218f00b960991b315cb7f
hullaah <umaradelo1.247@gmail.com>
2023-05-01 02:24:58 +0100

    added some files to folder 0x12

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print - prints  astring before the main function gets executed
+ * Return: void (does not have a return value)
+*/
+void print(void)
+{
+	char *quote = "You're beat! and yet, you must allow,";
+
+	printf("%s\nI bore my house upon my back!\n before the main", quote);
+}
