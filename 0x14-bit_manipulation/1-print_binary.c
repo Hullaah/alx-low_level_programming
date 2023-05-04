a64af64a4e1b3b489e671522a730d8e100ea0e26
hullaah <umaradelo1.247@gmail.com>
2023-05-04 12:35:55 +0100

    corrected some errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,8 +6,10 @@
 */
 void print_binary(unsigned long int n)
 {
-	if (n)
+	if (n > 1)
 		print_binary(n >> 1);
-	if (n)
-	_putchar((n & 1) + '0');
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
 }
