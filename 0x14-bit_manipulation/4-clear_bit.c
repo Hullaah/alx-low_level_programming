0e73f90581a0065d31c1dceb4fa1073020210ff5
hullaah <umaradelo1.247@gmail.com>
2023-05-04 15:52:29 +0100

    made file 4 betth compliant

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,16 @@
 #include "main.h"
-
+/**
+ * clear_bit - sets the bit at index to 0
+ * @n: number
+ * @index: index at which bit is located
+ * Return: 1 if succesful and -1 if failed
+*/
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i = 1, j = *n;
 	unsigned int k = 0;
 
-	if (index > sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	while (k != index)
 	{
