6ac297b938c00b9485d31574a0cb09042713cdab
hullaah <umaradelo1.247@gmail.com>
2023-05-04 15:46:54 +0100

    solved task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 1, j = *n;
+	unsigned int k = 0;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	while (k != index)
+	{
+		j >>= 1;
+		i <<= 1;
+		k++;
+	}
+	if (j & 1)
+		*n = *n - i;
+	return (1);
+}
