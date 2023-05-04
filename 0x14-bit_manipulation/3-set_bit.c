425545d936289bf15f7e662482fd7ec52645242b
hullaah <umaradelo1.247@gmail.com>
2023-05-04 15:04:37 +0100

    corrected some errors in fike 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,19 @@
 #include "main.h"
-
+/**
+ * set_bit - sets the bit at index to 1
+ * @n: number
+ * @index: index
+ * Return: -1 if set fails and 1 if successful0
+*/
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i = 1, j = *n;
 	unsigned int k = 0;
 
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
 	while (k != index)
 	{
 		j >>= 1;
