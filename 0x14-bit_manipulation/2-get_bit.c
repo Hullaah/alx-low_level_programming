0dbd7306d8004a47e2f88ca579cfbd9f588b59c7
hullaah <umaradelo1.247@gmail.com>
2023-05-04 13:15:50 +0100

    made the files betty compliant

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	int i = (n >> index) & 1;
 
 	if (index >= sizeof(unsigned long int) * 8)
-        	return (-1);
+		return (-1);
 	return (i);
 }
