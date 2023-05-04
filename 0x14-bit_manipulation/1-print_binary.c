9517dc7115a2825c57c28e9513bd216724be49c3
hullaah <umaradelo1.247@gmail.com>
2023-05-04 12:10:29 +0100

    made the first two files betty compliant

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,12 @@
 #include "main.h"
-
+/**
+ * print_binary - prints a number in binary using recursion
+ * @n: number to be printed in binary format
+ * Return: void (does not have a return value)
+*/
 void print_binary(unsigned long int n)
 {
-	if(n)
+	if (n)
 		print_binary(n >> 1);
 	if (n)
 	_putchar((n & 1) + '0');
