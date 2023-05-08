d0936ad16af7bef2d6e2f698f49ecd8bf011276f
hullaah <umaradelo1.247@gmail.com>
2023-05-08 11:35:48 +0100

    added fiule 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile - reads amount specified by letters from filename to stdout
+ * @filename: filename
+ * @letters: amount of bytes to read up to
+ * Return: amount of bytes written
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, red, written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+	red = read(fd, buffer, letters);
+	if (red == -1)
+		return (0);
+	written = write(STDIN_FILENO, buffer, red);
+	if (written == -1)
+		return (0);
+	free(buffer);
+	if (red != written)
+		return (0);
+	return (written);
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+# 0x15-file_io
+
+This is the directory for 0x15-file_io project done during the alx SE program
