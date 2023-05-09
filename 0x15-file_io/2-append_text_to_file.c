12c653a91f1400f33d6977b7754e453dd58c0176
hullaah <umaradelo1.247@gmail.com>
2023-05-09 14:47:40 +0100

    made another change

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,8 +22,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (!text_content)
+	if (!text_content && fd != -1)
 		return (1);
+	if (!text_content && fd == -1)
+		return (-1);
 	if (fd == -1)
 		return (-1);
 	written = write(fd, text_content, string_length);
