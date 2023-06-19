20d937df1e60fbb0799ab05b4e46f7c21b6169ac
hullaah <umaradelo1.247@gmail.com>
2023-06-19 13:55:52 +0100

    corrected fie 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic  -std=gnu89 -shared *.c -o liball.so -fPIC
+gcc -c -Wall -Werror -Wextra -pedantic  -std=gnu89 -fPIC *.c && gcc -shared -o liball.so *.o && rm -f *.o
