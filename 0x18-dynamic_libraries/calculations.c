dd9f2e557dbd06302e9c979ba943167acd2b65d6
hullaah <umaradelo1.247@gmail.com>
2023-06-20 19:21:46 +0100

    solved task 100

--- 0x18-dynamic_libraries/calculations.c
+++ 0x18-dynamic_libraries/calculations.c
@@ -1,51 +1,50 @@
-#include <python3.10/Python.h>
 /**
- * op_add - adds two numbers together.
+ * add - adds two numbers together.
  * @a: number
  * @b: number
  * Return: sum of numbers
 */
-PyObject *add(PyObject *a, PyObject *b)
+int add(int a, int b)
 {
-        return (PyNumber_Add(a, b));
+        return (a + b);
 }
 /**
- * op_sub - subtracts two numbers from each other
+ * sub - subtracts two numbers from each other
  * @a: number
  * @b: number
  * Return: difference of numbers
 */
-PyObject *sub(PyObject *a, PyObject *b)
+int sub(int a, int b)
 {
-	return (PyNumber_Subtract(a, b));
+	return (a - b);
 }
 /**
- * op_mul - multiplies two numbers together.
+ * mul - multiplies two numbers together.
  * @a: number
  * @b: number
  * Return: product of numbers
 */
-PyObject *mul(PyObject *a, PyObject *b)
+int mul(int a, int b)
 {
-	return (PyNumber_Multiply(a, b));
+	return (a * b);
 }
 /**
- * op_div - divides a by b
+ * div - divides a by b
  * @a: number
  * @b: number
  * Return: division of numbers
 */
-PyObject *divide(PyObject *a, PyObject *b)
+int divide(int a, int b)
 {
-	return (PyNumber_TrueDivide(a, b));
+	return (a / b);
 }
 /**
- * op_mod - gets the remainder of a when divided by b.
+ * mod - gets the remainder of a when divided by b.
  * @a: number
  * @b: number
  * Return: modulus of numbers
 */
-PyObject *mod(PyObject *a, PyObject *b)
+int mod(int a, int b)
 {
-	return (PyNumber_Remainder(a, b));
+	return (a % b);
 }
