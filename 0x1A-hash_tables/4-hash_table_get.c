15cf70e4eac42b7fea35083f402dd592c4643813
hullaah <umaradelo1.247@gmail.com>
2023-07-07 16:30:26 +0100

    solved betty issues

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,8 +10,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *ptr;
 
-        if (!ht)
-                return (NULL);
+	if (!ht)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	ptr = ht->array[index];
 	while (ptr)
